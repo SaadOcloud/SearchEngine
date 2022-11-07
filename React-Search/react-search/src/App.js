bc4396d9a1e6f88067aa56509cb5e85fa502e350
Saad Fareed <113437351+SaadOcloud@users.noreply.github.com>
2022-11-07 11:28:11 +0500

    Search Bar with Two Checkoboxes is added

--- React-Search/react-search/src/App.js
+++ React-Search/react-search/src/App.js
@@ -2,12 +2,16 @@ import { React, useState } from "react";
 import TextField from "@mui/material/TextField";
 import List from "./Components/List";
 import "./App.css";
-
 function App() {
   const [inputText, setInputText] = useState("");
-  let inputHandler = (e) => {
+  const [checkboxdata, setcheckboxdata] = useState([]);
+  const inputHandler = (e) => {
     var lowerCase = e.target.value.toLowerCase();
     setInputText(lowerCase);
+  };
+  const checkboxHandler = (e) => {
+    var checkbox = e.target.value;
+    setcheckboxdata(checkboxdata.push(checkbox));
   };
 
   return (
@@ -15,12 +19,19 @@ function App() {
       <h1>React Search</h1>
       <div className="search">
         <TextField
+          className="search-bar"
           id="outlined-basic"
           onChange={inputHandler}
           variant="outlined"
           fullWidth
           label="Search"
         />
+      </div>
+      <div className="footer">
+        <input type="checkbox" />
+        <label for="filter"> Select All</label>
+        <input type="checkbox" />
+        <label for="filter"> Clear All</label>
       </div>
       <List input={inputText} />
     </div>
